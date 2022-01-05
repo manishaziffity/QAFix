c85cd058ffedd584c03fff760127e76426bf75fe
manisha2593 <manisha.mandaliya@nettyfy.com>
2022-01-05 18:28:46 +0530

    Code Optimize

--- js/app.js
+++ js/app.js
@@ -3,34 +3,11 @@ $(document).ready(function () {
   $("#btn-search").on("click", function (e) {
     e.preventDefault();
     localStorage.clear(); //Clears storage for next request
-    email = $('input[type="text"]').val().toLowerCase();
-
-    var x, y;
-    regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    let email = $('input[type="text"]').val().toLowerCase();
+    let regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
     if (email.match(regEx)) {
-      x = true;
+      getEmailData(email);
     } else {
-      x = false;
-    }
-
-    if (x === true) {
-
-      $(".hide-content").hide();
-      $(".loader").show();
-      document.querySelector('input[type="text"]').parentNode.classList.remove("error");
-      const proxyurl = "";
-      const url =
-        'https://ltv-data-api.herokuapp.com/api/v1/records.json?email=' + email;
-      fetch(proxyurl + url)
-        .then((response) => response.text())
-        .then(function (contents) {
-          localStorage.setItem("userObject", contents);
-          setTimeout(function () {
-            window.location.href = "result.html";
-          }, 1000);
-        })
-        .catch((e) => console.log(e));
-    } else if (x !== true) {
       document.querySelector('input[type="text"]').parentNode.classList.add("error");
     }
   });
@@ -40,59 +17,37 @@ $(document).ready(function () {
     localStorage.clear(); //Clears storage for next request
     let phoneNumber = $('input[name="phone_number"]').val();
     if (phoneNumber != '' && phoneNumber.length == 10) {
-      $(".hide-content").hide();
-      $(".loader").show();
-      const proxyurl = "";
-      const url =
-        'https://ltv-data-api.herokuapp.com/api/v1/records.json?phone=' + phoneNumber;
-      fetch(proxyurl + url)
-        .then((response) => response.text())
-        .then(function (contents) {
-          localStorage.setItem("userObject", contents);
-          setTimeout(function () {
-            window.location.href = "result.html";
-          }, 1000);
-        })
-        .catch((e) => console.log(e));
+      getMobileData(phoneNumber);
     } else {
       document.querySelector('input[name="phone_number"]').parentNode.classList.add("error-phone");
     }
 
   });
   $('input[type="text"]').keypress(function (event) {
-    email = $('input[type="text"]').val().toLowerCase();
-    regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-    if (email.match(regEx)) {
-      x = true;
-      document.querySelector('input[type="text"]').parentNode.classList.remove("error");
-    } else {
-      x = false;
-    }
-    keycode = (event.keyCode ? event.keyCode : event.which);
+    let email = $('input[type="text"]').val().toLowerCase();
+    let regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    let keycode = (event.keyCode ? event.keyCode : event.which);
     if (keycode == '13') {
-      /**
-       * Makes a request to ltv API to search an specific email address.
-       * If there's a response, it gets stored in the local storage and redirects to results page
-       */
       event.preventDefault();
-      localStorage.clear(); //Clears storage for next request
-
-      var x, y;
-
-
-      if (x === true) {
-        const proxyurl = "";
-        const url =
-          'https://ltv-data-api.herokuapp.com/api/v1/records.json?email=' + email;
-        fetch(proxyurl + url)
-          .then((response) => response.text())
-          .then(function (contents) {
-            localStorage.setItem("userObject", contents);
-            window.location.href = "result.html";
-          })
-          .catch((e) => console.log(e));
-      } else if (x !== true) {
+      localStorage.clear();
+      if (email.match(regEx)) {
+        getEmailData(email);
+      } else {
         document.querySelector('input[type="text"]').parentNode.classList.add("error");
+      }
+    }
+  });
+
+  $('input[name="phone_number"]').keypress(function (event) {
+    let phoneNumber = $('input[name="phone_number"]').val();
+    let keycode = (event.keyCode ? event.keyCode : event.which);
+    if (keycode == '13') {
+      event.preventDefault();
+      localStorage.clear();
+      if (phoneNumber != '' && phoneNumber.length == 10) {
+        getMobileData(phoneNumber);
+      } else {
+        document.querySelector('input[name="phone_number"]').parentNode.classList.add("error-phone");
       }
     }
   });
@@ -100,6 +55,8 @@ $(document).ready(function () {
   $(".change-tab").click(function () {
     $('.change-tab').removeClass('tab-button-active');
     $(this).addClass("tab-button-active");
+    $('.change-tab').parent().removeClass('tab-active');
+    $(this).parent().addClass("tab-active");
     if ($(this).val() != 'email') {
       $(".phone-section").show();
       $(".email-section").hide();
@@ -110,3 +67,39 @@ $(document).ready(function () {
   });
 
 });
+
+function getEmailData(email) {
+  $(".hide-content").hide();
+  $(".loader").show();
+  document.querySelector('input[type="text"]').parentNode.classList.remove("error");
+  const proxyurl = "";
+  const url =
+    'https://ltv-data-api.herokuapp.com/api/v1/records.json?email=' + email;
+  fetch(proxyurl + url)
+    .then((response) => response.text())
+    .then(function (contents) {
+      localStorage.setItem("userObject", contents);
+      setTimeout(function () {
+        window.location.href = "result.html";
+      }, 1000);
+    })
+    .catch((e) => console.log(e));
+}
+
+function getMobileData(phoneNumber) {
+  document.querySelector('input[name="phone_number"]').parentNode.classList.remove("error-phone");
+  $(".hide-content").hide();
+  $(".loader").show();
+  const proxyurl = "";
+  const url =
+    'https://ltv-data-api.herokuapp.com/api/v1/records.json?phone=' + phoneNumber;
+  fetch(proxyurl + url)
+    .then((response) => response.text())
+    .then(function (contents) {
+      localStorage.setItem("userObject", contents);
+      setTimeout(function () {
+        window.location.href = "result.html";
+      }, 1000);
+    })
+    .catch((e) => console.log(e));
+}
